9d926fe116e527979ef3934064e4827b6fd87d78
Sam Shih <sam810331@gmail.com>
2020-08-13 14:28:48 +0800

    wording update

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const captcha = require('./lib/captcha');
 
 program
     .command('render')
-    .description('Render captcha picture.')
+    .description('Render captcha picture')
     .option('-l, --length [length]', 'Specify the length of captcha.', 4)
     .option('-p, --pattern [pattern]', 'd: all digit, a: all alphabet, da: digit & alphabet.', 'da')
     .option('-c, --case [case]', 'u: upper case, l: lower case, ci: case insensitive.', 'ci')
     .option('--width [width]', 'Specify the width of image.', 100)
     .option('--height [height]', 'Specify the height of image.', 40)
-    .option('-o, --output [output]', 'Specify the picture\'s output path.')
+    .option('-o, --output [output]', 'Write to a file, instead of STDOUT')
     .action(captcha.renderCaptcha);
 
 program
     .command('serve')
-    .description('Render captcha picture')
+    .description('Start a web server to serve RESTful render api')
     .option('-l, --length [length]', 'Specify the length of captcha.', 4)
     .option('-p, --pattern [pattern]', 'd: all digit, a: all alphabet, da: digit & alphabet.', 'da')
     .option('-c, --case [case]', 'u: upper case, l: lower case, ci: case insensitive.', 'ci')
@@ -26,9 +26,9 @@ program
     .option('--port [port]', 'Specify server port.', 80)
     // .option('--host [host]', 'Specify server host.', '0.0.0.0')
     .option('-t ,--ttl [ttl]', 'Expire time of redis key(unit: seconds).', 60)
-    .option('--redis-port [redis-port]', 'Redis port', 6379)
-    .option('--redis-host [redis-host]', 'Redis host', '127.0.0.1')
-    .option('--redis-password [redis-password]', 'Redis password')
+    .option('--redis-port [redis-port]', 'redis port', 6379)
+    .option('--redis-host [redis-host]', 'redis host', '127.0.0.1')
+    .option('--redis-password [redis-password]', 'redis password')
     .action(captcha.serve);
 
 program
